c0aabec6efd5f79d2d9bf401c5a08160a408e3af
noviambar <ambarwatinovi1@gmail.com>
2022-03-22 13:51:16 +0700

    Melakukan Perubahan pada File Name

--- Pertemuan2/Name.js
+++ Pertemuan2/Name.js
@@ -1,6 +1,12 @@
-var name ='Novi Ambar Wati'
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-var subject = `Hello My Name is ${name}`
- 
-console.log(subject)
-
+readline.question('Siapa Nama Kamu?', name=> {
+    readline.question('Bagaimana Kabar Kamu Hari ini?', kabar=> {
+        readline.question('Apakah kamu bahagia?', kondisi=>{
+            console.log(`Nama Saya ${name}, Kabar saya hari ini ${kabar}, Hari ini Saya ${kondisi}`)
+        } )
+    })
+})
