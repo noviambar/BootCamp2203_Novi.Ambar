391f4c915631305649dc2a77e78d06d447168d27
noviambar <ambarwatinovi1@gmail.com>
2022-03-24 11:41:05 +0700

    Update File app.js dan contact.js

--- 5.Pertemuan4_ContactCode/contacts.js
+++ 5.Pertemuan4_ContactCode/contacts.js
@@ -9,6 +9,16 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+const saveContact = (name,email,mobile) => {
+    const contact = {name,mobile,email}
+    const file = fs.readFileSync('Data/contacts.json','utf8')
+    const contacts = JSON.parse(file)
+    contacts.push(contact)
+    fs.writeFileSync('Data/contacts.json', JSON.stringify(contacts))
+    console.log('Terima Kasih sudah memasukkan data!')
+            rl.close()
+}
+module.exports.saveContact = saveContact
 
 const questions = (ask) => {
     return new Promise((resolve,reject)=>{
@@ -21,27 +31,27 @@ module.exports.questions = questions
 
 
 
-const main = async()=>{
-    const name = await questions('What is your name?')
-    const mobile = await questions('Your mobile phone?')
-    const email = await questions('Your Email?')
-    const contact = {name,mobile,email}
-    //Membuat folder data apabila tidak ada
-    if(!fs.existsSync(dirPath)){
-        fs.mkdirSync(dirPath)
-    }
-    //membuat file contacts.json jika belum ada
-    if(!fs.existsSync(dataPath)){
-        fs.writeFileSync(dataPath,'[]','utf-8')
-    }
-    const file = fs.readFileSync('Data/contacts.json','utf8')
-    const contacts = JSON.parse(file)
-    contacts.push(contact)
-    fs.writeFileSync('Data/contacts.json', JSON.stringify(contacts))
-    console.log('Terima Kasih sudah memasukkan data!')
-    rl.close()
-}
-main()
+// const main = async()=>{
+//     const name = await questions('What is your name?')
+//     const mobile = await questions('Your mobile phone?')
+//     const email = await questions('Your Email?')
+//     const contact = {name,mobile,email}
+//     //Membuat folder data apabila tidak ada
+//     if(!fs.existsSync(dirPath)){
+//         fs.mkdirSync(dirPath)
+//     }
+//     //membuat file contacts.json jika belum ada
+//     if(!fs.existsSync(dataPath)){
+//         fs.writeFileSync(dataPath,'[]','utf-8')
+//     }
+//     const file = fs.readFileSync('Data/contacts.json','utf8')
+//     const contacts = JSON.parse(file)
+//     contacts.push(contact)
+//     fs.writeFileSync('Data/contacts.json', JSON.stringify(contacts))
+//     console.log('Terima Kasih sudah memasukkan data!')
+//     rl.close()
+// }
+// main()
 
 // rl.question('What is yor name? ', (name)=>{
 //     rl.question('Your mobile number?',(mobile)=>{
