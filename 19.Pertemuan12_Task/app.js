5a1418246cb137f5360541334d3018f6b6114d3b
noviambar <ambarwatinovi1@gmail.com>
2022-04-05 11:19:46 +0700

    Task 1 -Detail Contact-  Pertemuan 12

---
+++ 19.Pertemuan12_Task/app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const morgan = require('morgan')
+const { argv } = require('yargs')
+const app = express()
+const contacts = require('./contacts') 
+const port = 3000
+
+app.use(expressLayouts)
+
+app.use(morgan('dev'))
+
+//using middleware
+app.use((req, res,next) => {
+    console.log('Time:', Date.now())
+    next()
+})
+
+//information using ejs
+app.set('view engine','ejs')
+
+
+app.get('/',(req,res)=>{
+    const nama = "Novi Ambar Wati"
+    const title = "Home Page"
+    
+    res.render('index',{nama, title})
+})
+
+app.use(express.static('public'))
+
+app.get('/About',(req,res)=>{
+    const title = "About Page"
+    cont =[]
+    
+    res.render('about',{title, cont})
+})
+
+app.get('/Contact',(req,res)=>{
+    const title = "Contact Page"
+    const cont = contacts.loadContact()
+    
+    res.render('contact',{title, cont})
+})
+
+app.get('/Detail/:name',(req,res)=>{
+    const title = "Detail Page"
+    // const cont = contacts.loadContact()
+    // const detail = contacts.detailContact()
+    const contact = contacts.findContact(req.params.name)
+    console.log(contact)
+
+    res.render('detail',{title, contact})
+})
+
+
+app.get('/Product/:id', (req, res) => {
+    
+    res.send('Product: ' + req.params.id + '<br></br>' + 'Category: ' + req.query.category)
+
+})
+
+app.use('/',(req,res)=>{
+    res.status(404).send('Page Not Found!: 404')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
